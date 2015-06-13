292d78f5a9b8fd1ff34402fac68efade60f0682d
Justin Spradlin <jspradlin@gmail.com>
2015-06-12 21:07:15 -0400

    Changing the case and formatting of some things to be more consistent with what is likely to be expected.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_spokespot-api_session'
+Rails.application.config.session_store :cookie_store, key: '_spokespot_api_session'
